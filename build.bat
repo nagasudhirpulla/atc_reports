98b04e3487d489a967a031dbe6bc36428f3edd0c
unknown <nagasudhirpulla@gmail.com>
2021-07-12 12:31:00 +0530

    dist folder rename

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call activate_env.bat
-call pyinstaller index.py --onefile
-call xcopy /Y templates\report_template.docx dist\report_template.docx 
+call pyinstaller index.py --onefile --distpath ./atc_reports_dist
+call xcopy /Y templates\report_template.docx atc_reports_dist\report_template.docx 
